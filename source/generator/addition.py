5fd241e64fb6edfafd7b6d8ce8f2c093239aae79
«Az2Poivron» <monnomctom@gmail.com>
2021-05-29 19:28:33 +0200

    Second try

---
+++ source/generator/addition.py
@@ -0,0 +1,21 @@
+from random import randint as rdm
+
+class Addition:
+    
+    def __init__(self,b_min,b_max,equa=False):
+        #a + b = c
+        a = rdm(b_min,b_max) 
+        b = rdm(b_min,b_max) 
+        c = a + b
+
+        L = [a,b,c]
+
+        if equa:
+            a_index = rdm(0,2)
+        else:
+            a_index = 2
+
+        self.answer = L[a_index]
+        L[a_index] = "?"
+
+        self.str = f"{L[0]} + {L[1]} = {L[2]}"
